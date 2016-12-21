891ce961f6d97a5536eaed3e6fbf7b3eb92f4034
civic1987 <rosenbaum1987@gmail.com>
2016-12-21 18:08:59 +0200

    Everything is ok

---
+++ src/App/pages/places/places-components/places-map.vue
@@ -0,0 +1,21 @@
+<template>
+    <section>
+        <div>This is Places Map</div>
+    </section>
+
+</template>
+
+<script>
+export default {
+        data(){
+            return {
+              
+            }
+        }
+}
+
+</script>
+
+<style>
+
+</style>
---
+++ src/App/pages/places/places-components/places-preview.vue
@@ -0,0 +1,21 @@
+<template>
+    <section>
+        <div>This is Places preview</div>
+    </section>
+
+</template>
+
+<script>
+export default {
+        data(){
+            return {
+              
+            }
+        }
+}
+
+</script>
+
+<style>
+
+</style>
