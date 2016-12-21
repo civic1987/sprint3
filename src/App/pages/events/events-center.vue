d930cbaca61b2ccf4a7f5ba714d41695002a3d46
civic1987 <rosenbaum1987@gmail.com>
2016-12-21 18:01:38 +0200

    Added Events Components

--- src/App/pages/events/events-center.vue
+++ src/App/pages/events/events-center.vue
@@ -2,17 +2,38 @@
     <section>
         <div>
   <h1>Events Component</h1>
+  <events-details></events-details>
+  <events-edit></events-edit>
+  <events-filter></events-filter>
+  <events-group></events-group>
+  <events-list></events-list>
+  <events-preview></events-preview>
 </div>
     </section>
 </template>
 
 <script>
+import EventsDetails from './events-components/events-details.vue'
+import EventsEdit from './events-components/events-edit.vue'
+import EventsFilter from './events-components/events-filter.vue'
+import EventsGroup from './events-components/events-group.vue'
+import EventsList from './events-components/events-list.vue'
+import EventsPreview from './events-components/events-preview.vue'
+
     export default  {
-  data: () => {
+  data() {
     return {
       
     }
-  }
+  },
+  components:{
+        'events-details' : EventsDetails,
+        'events-edit' : EventsEdit,
+        'events-filter' : EventsFilter,
+        'events-group' : EventsGroup,
+        'events-list' : EventsList,
+        'events-preview' : EventsPreview,
+    }
 }
 </script>
 
---
+++ src/App/pages/events/events-components/events-details.vue
@@ -0,0 +1,23 @@
+<template>
+<section>
+    <div>This is Details</div>
+</section>
+
+</template>
+
+<script>
+export default {
+    
+        data(){
+            return {
+              
+            }
+        }
+}
+
+</script>
+
+<style>
+
+
+</style>
