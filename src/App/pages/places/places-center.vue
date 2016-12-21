f3f74100136bdbab7fc6e0616d427594a325eee5
Dor Banay <banaydor@gmail.com>
2016-12-21 18:04:25 +0200

    Added Places Components

--- src/App/pages/places/places-center.vue
+++ src/App/pages/places/places-center.vue
@@ -1,24 +1,39 @@
 <template>
     <section>
         <div>
-  <h1>Places Component</h1>
-</div>
+            <h1>Places Component</h1>
+        </div>
+        <places-list></places-list>
+        <places-details></places-details>
+        <places-map></places-map>
+        <places-preview></places-preview>
     </section>
 </template>
 
 <script>
+import PlacesList from './places-components/places-list'
+import PlacesDetails from './places-components/places-details'
+import PlacesPreview from './places-components/places-preview'
+import PlacesMap from './places-components/places-map'
+
+
     export default  {
-  data: () => {
+  data() {
     return {
       
     }
+  },
+  components:{
+      'places-list': PlacesList,
+      'places-details': PlacesDetails,
+      'places-preview': PlacesPreview,
+      'places-map': PlacesMap
   }
 }
 </script>
 
 <style>
-h1{
-    color:orange;
-}
-
+    h1 {
+        color: orange;
+    }
 </style>
